a7319f65026585afe1ec6277553f107a9eb3f5d3
m0-SakamotoYuta <92511394+m0-SakamotoYuta@users.noreply.github.com>
2025-06-20 19:16:36 +0900

    Reorganize files into scripts and data directories

---
+++ data/pv.pp
@@ -0,0 +1,15 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="19:41:13" date="2025-05-14"/>
+  <User name="bio"/>
+  <DataFileName name="Scan.obj"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point active="1" name="pvA" z="418.14514" x="-173.37123" y="-0.012757447"/>
+ <point active="1" name="pvB" z="494.40964" x="51.580944" y="-6.3162918"/>
+ <point active="1" name="pvC" z="579.46942" x="-58.014404" y="-65.310944"/>
+ <point active="1" name="pvD" z="554.75421" x="-135.80382" y="-61.689251"/>
+ <point active="1" name="pvE1" z="479.49762" x="-168.1012" y="29.523296"/>
+ <point active="1" name="pvE2" z="526.44348" x="-155.75357" y="59.562656"/>
+</PickedPoints>
